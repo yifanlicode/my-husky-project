580a8d667a86b9336c085e811a6c83b5ccd325e1
yifanli <yifanlicode@gmail.com>
2023-07-08 16:59:32 -0500

    test

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import { ButtonProps } from "./Button.types";
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { ButtonProps } from './Button.types';
 
 interface StyledButtonProps extends ButtonProps {
   disabled: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  size = "medium",
+  size = 'medium',
   primary = false,
   disabled = false,
-  text = "Button",
+  text = 'Buttons',
   onClick,
 }) => {
-  const defaultTextColor = "#333333";
-  const defaultBackgroundColor = "#f2f2f2";
-  const primaryTextColor = "#ffffff";
-  const primaryBackgroundColor = "#7bbcf8";
-  const hoverBackgroundColor = "#55bd90";
-  const activeBorderColor = "#1b116e";
+  const defaultTextColor = '#333333';
+  const defaultBackgroundColor = '#f2f2f2';
+  const primaryTextColor = '#ffffff';
+  const primaryBackgroundColor = '#7bbcf8';
+  const hoverBackgroundColor = '#55bd90';
+  const activeBorderColor = '#1b116e';
 
   const StyledButton = styled.button<StyledButtonProps>`
     border: 0;
@@ -29,11 +29,11 @@ const Button: React.FC<ButtonProps> = ({
     border-radius: 4px;
     display: inline-block;
     padding: ${(props) =>
-      props.size === "small"
-        ? "8px 20px"
-        : props.size === "large"
-        ? "14px 30px"
-        : "10px 24px"};
+      props.size === 'small'
+        ? '8px 20px'
+        : props.size === 'large'
+        ? '14px 30px'
+        : '10px 24px'};
     color: ${(props) => (props.primary ? primaryTextColor : defaultTextColor)};
     background-color: ${(props) =>
       props.primary ? primaryBackgroundColor : defaultBackgroundColor};
@@ -48,17 +48,17 @@ const Button: React.FC<ButtonProps> = ({
 
     &:hover {
       background-color: ${(props) =>
-        props.primary ? hoverBackgroundColor : "#e6e6e6"};
+        props.primary ? hoverBackgroundColor : '#e6e6e6'};
     }
 
     &:active {
       border: solid 2px ${activeBorderColor};
       padding: ${(props) =>
-        props.size === "small"
-          ? "6px 18px"
-          : props.size === "large"
-          ? "12px 26px"
-          : "8px 22px"};
+        props.size === 'small'
+          ? '6px 18px'
+          : props.size === 'large'
+          ? '12px 26px'
+          : '8px 22px'};
     }
   `;
 
